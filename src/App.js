15a0945231a94d80b607e0d794e0aaa174fe6939
honzukka <randomnonplayercharacter@gmail.com>
2020-08-21 20:55:56 +0300

    html view ready for mock data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 import Data from './data';
 
-import Container from 'react-bootstrap/Container';
-
-
+// TODO: the Accordion element definition is scattered across multiple functions...
+// TODO: would loading the body data only on click make things faster? (callback?)
+//    - https://reactjs.org/docs/optimizing-performance.html#virtualize-long-lists
 
 class App extends React.Component {
   constructor(props) {
@@ -16,42 +21,98 @@ class App extends React.Component {
   render() {
     return (
       <Container fluid className="p-3">
-        <List packages={this.state.packages}/>
+        <Accordion>
+          <List packages={this.state.packages}/>
+        </Accordion>
       </Container>
     );
   }
 }
 
-// FIXME: package info should unfold on click
-class List extends React.Component {
-  render() {
-    let items = [];
-    for (let [packageName, packageData] of this.props.packages) {
-      items.push(
-        <ul key={packageName}>
-          <Item
-            name={packageName}
-            description={packageData.description}
-            dependencies={packageData.dependencies}
-            reverseDependencies={packageData.reverseDependencies}
-          />
-        </ul>
-      );
-    }
-    return items;
+function List(props) {
+  let items = [];
+  for (let [packageName, packageData] of props.packages) {
+    items.push(
+      <Item key={packageName}
+        name={packageName}
+        data={packageData}
+      />
+    );
   }
+  return items;
 }
 
-// FIXME: only dependencies that are installed should be clickable ('|' character...)
 function Item(props) {
   return (
-    <li>
-      <h1 id={props.name}>{props.name}</h1>
-      <p>{props.description}</p>
-      <p><b>Dependencies: </b>{props.dependencies.map((dep) => <a key={props.name+dep} href={"#"+dep}>{dep}, </a>)}</p>
-      <p><b>Reverse dependencies: </b>{props.reverseDependencies.map((dep) => <a key={props.name+dep} href={"#"+dep}>{dep}, </a>)}</p>
-    </li>
+    <Card>
+      <Card.Header>
+        <PackageLinkHeader name={props.name} />
+      </Card.Header>
+      <Accordion.Collapse eventKey={props.name}>
+        <Card.Body>
+          <PackageTitle name={props.name} />
+          <PackageDescription description={props.data.description} />
+          <PackageDependencies deps={props.data.dependencies} />
+          <PackageReverseDependencies deps={props.data.reverseDependencies} />
+        </Card.Body>
+      </Accordion.Collapse>
+    </Card>
   );
+}
+
+function PackageTitle(props) {
+  return <h1 className="mb-3">{props.name}</h1>;
+}
+
+function PackageDescription(props) {
+  return <p className="mb-3">{props.description}</p>;
+}
+
+function PackageDependencies(props) {
+  return (
+    <>
+      <p><b>Dependencies: </b></p>
+      <p><DependencyList deps={props.deps}></DependencyList></p>
+    </>
+  );
+}
+
+function PackageReverseDependencies(props) {
+  return (
+    <>
+      <p><b>Reverse dependencies: </b></p>
+      <p><DependencyList deps={props.deps}></DependencyList></p>
+    </>
+  );
+}
+
+function DependencyList(props) {
+  return props.deps.map(dep => <PackageLinkDependency key={dep.name} dep={dep}/>);
+}
+
+function PackageLinkHeader(props) {
+  return (
+      <Accordion.Toggle as={Button} variant="link" eventKey={props.name}>
+        {props.name}
+      </Accordion.Toggle>
+  );
+}
+
+function PackageLinkDependency(props) {
+  const buttonClass = "mr-1 mt-1";
+  if (props.dep.listed) {
+    return (
+      <Accordion.Toggle as={Button} variant="secondary" className={buttonClass} eventKey={props.dep.name}>
+        {props.dep.name}
+      </Accordion.Toggle>
+    );
+  } else {
+    return (
+      <Button key={props.dep.name} variant="light" className={buttonClass}>
+        {props.dep.name}
+      </Button>
+    )
+  }
 }
 
 export default App;
